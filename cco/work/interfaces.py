a65c2618dcbccdaa7cb493ba902ae99301e0a318
Helmut Merz <helmutm@cy55.de>
2025-01-09 17:59:17 +0100

    Python3 fix in cco.work

--- cco/work/interfaces.py
+++ cco/work/interfaces.py
@@ -1,23 +1,6 @@
-#
-#  Copyright (c) 2017 Helmut Merz helmutm@cy55.de
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
+# cco.work.interfaces 
 
-"""
-Interfaces for organizational stuff like persons and addresses.
+""" Interfaces for organizational stuff like persons and addresses.
 """
 
 from zope.i18nmessageid import MessageFactory
@@ -47,7 +30,7 @@ class DurationFieldInstance(FieldInstance):
     def display(self, value):
         if value is None:
             return ''
-        if isinstance(value, basestring):
+        if isinstance(value, str):
             value = value.replace(',', '.')
         try:
             value = float(value)
