0690728ec1c1e1236d98b41837cba74a7a18d759
Helmut Merz <helmutm@cy55.de>
2025-02-22 10:13:19 +0100

    Py3 fix: default for Bytes field b''

--- cco/common/interfaces.py
+++ cco/common/interfaces.py
@@ -1,23 +1,6 @@
-#
-#  Copyright (c) 2017 Helmut Merz helmutm@cy55.de
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
+# cco.common.interfaces
 
-"""
-Interfaces for the cco.common package.
+""" Interfaces for the cco.common package.
 """
 
 from zope.app.container.constraints import contains, containers
@@ -73,8 +56,8 @@ class ISharepointLink(IResourceAdapter):
     data = schema.Bytes(
         title=_(u'Data'),
         description=_(u'Resource raw data'),
-        default='',
-        missing_value='',
+        default=b'',
+        missing_value=b'',
         required=False)
 
     contentType = schema.Choice(
